74fb498676aeb34ef52b27c694acc15e2800c877
Mergen Battur <mbattur@yahoo.com>
2015-12-17 18:06:20 -0600

    completed CRUD

--- spec/controllers/wikis_controller_spec.rb
+++ spec/controllers/wikis_controller_spec.rb
@@ -66,4 +66,61 @@ RSpec.describe WikisController, type: :controller do
         expect(assigns(:wiki)).to eq(my_wiki)
       end
     end
+
+    describe "GET edit" do
+      it "returns http success" do
+        get :edit, {id: my_wiki.id}
+        expect(response).to have_http_status(:success)
+      end
+
+      it "renders the #edit view" do
+        get :edit, {id: my_wiki.id}
+        expect(response).to render_template :edit
+      end
+
+      it "assigns wiki to be updated to @wiki" do
+        get :edit, {id: my_wiki.id}
+
+        wiki_instance = assigns(:wiki)
+
+        expect(wiki_instance.id).to eq my_wiki.id
+        expect(wiki_instance.title).to eq my_wiki.title
+        expect(wiki_instance.body).to eq my_wiki.body
+      end
+    end
+
+    describe "PUT update" do
+      it "updates wiki with expected attributes" do
+        new_title = RandomData.random_sentence
+        new_body = RandomData.random_paragraph
+
+        put :update, id: my_wiki.id, wiki: {title: new_title, body: new_body}
+
+        updated_wiki = assigns(:wiki)
+        expect(updated_wiki.id).to eq my_wiki.id
+        expect(updated_wiki.title).to eq new_title
+        expect(updated_wiki.body).to eq new_body
+      end
+
+      it "redirects to the updated wiki" do
+        new_title = RandomData.random_sentence
+        new_body = RandomData.random_paragraph
+
+        put :update, id: my_wiki.id, wiki: {title: new_title, body: new_body}
+        expect(response).to redirect_to my_wiki
+      end
+    end
+
+    describe "DELETE destroy" do
+     it "deletes the wiki" do
+       delete :destroy, {id: my_wiki.id}
+       count = Wiki.where({id: my_wiki.id}).size
+       expect(count).to eq 0
+     end
+ 
+     it "redirects to wikis index" do
+       delete :destroy, {id: my_wiki.id}
+       expect(response).to redirect_to wikis_path
+     end
+   end
 end
