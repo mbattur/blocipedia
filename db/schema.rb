ba52e9cbefb6b75cee08f061502e7e44053df67d
Mergen Battur <mbattur@yahoo.com>
2015-12-19 12:37:15 -0600

    fix schema

--- db/schema.rb
+++ db/schema.rb
@@ -31,9 +31,8 @@ ActiveRecord::Schema.define(version: 20151216190646) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "wikis", ["user_id"], name: "index_wikis_on_user_id"
 
-<<<<<<< HEAD
-=======
   create_table "wikis", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
@@ -42,8 +41,4 @@ ActiveRecord::Schema.define(version: 20151216190646) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
-  add_index "wikis", ["user_id"], name: "index_wikis_on_user_id"
-
->>>>>>> userstory3-wiki
 end
