ea1a173f4005a16c1310c3af55aa05f8916ae7d4
Mergen Battur <mbattur@yahoo.com>
2015-12-26 13:44:12 -0600

    added role to db

---
+++ app/policies/wiki_policy.rb
@@ -0,0 +1,36 @@
+class WikiPolicy < ApplicationPolicy
+  attr_reader :user, :wiki
+
+  def initialize(user, wiki)
+    @user = user
+    @wiki = wiki
+  end
+
+  def index?
+    true
+  end
+
+  def show?
+    true
+  end
+
+  def new?
+    true
+  end
+
+  def create?
+    true
+  end
+
+  def edit?
+    true
+  end
+
+  def update?
+    user.admin? or not wiki.published?
+  end
+
+  def destroy?
+    true
+  end
+end
