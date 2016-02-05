4b7fccfbee6b16225eceb4334002f274177bc9d4
Mergen Battur <mbattur@yahoo.com>
2016-02-04 21:17:00 -0600

    mailtrap prod fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,6 +77,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  Rails.application.routes.default_url_options[:host] = 'salty-citadel-6718.herokuapp.com'
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :user_name => '57bb8a3de9ada9',
