c7e92124b38cbab0108dcc35705df40658f9015b
Mergen Battur <mbattur@yahoo.com>
2016-02-08 11:56:06 -0600

    interface ready

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get 'welcome/index'
 
   resources :wikis
-  resources :users, only: [:show]
+  resources :users#, only: [:show, :index]
   resources :welcome, only: [:index, :about]
 
   get 'about' => 'welcome#about'
