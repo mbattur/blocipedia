fa3c59b743c61484058cf29aed86987f628351ef
Mergen Battur <mbattur@yahoo.com>
2016-02-25 13:33:09 -0600

    first 2 tests pass

--- spec/controllers/charges_controller_spec.rb
+++ spec/controllers/charges_controller_spec.rb
@@ -1,12 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe ChargesController, type: :controller do
+  before (:each) do
+    @user = FactoryGirl.create(:user)
+    sign_in @user
+  end
 
-  # describe "GET #create" do
-  #   it "returns http success" do
-  #     get :create
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
+  it "checks users role is standard" do
+    post :create
+    expect(@user.role).to eq('standard')
+  end
 
+  it "checks updated role is premium" do
+    post :create
+    updated_user = assigns(:user)
+    my_role = @user.role
+    expect(updated_user.role).to eq my_role
+  end
 end
+
+    #setup
+
+      #setup my user
+      #user needs to be signed in as standard
+
+    #experiment
+      #invoke ChargesController create action
+      #pass the information it needs (rake routes)
+
+    #verify
+      #verify that the user is now premium
